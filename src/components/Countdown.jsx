be3ba86235a7b35458304097c217bc636cfeecc3
tabassumt121-web <tabassumt121@gmail.com>
2026-02-13 19:53:44 +0530

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -76,7 +76,7 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
       <section className="countdown">
         <div className="flip-timer">
           <span className="birthday-celebration">
-            🎉 It's Your Birthday! 🎉
+            💘 It's Your Birthdayyyyy! 🎀
           </span>
         </div>
       </section>
